73c3f15880fde3ed199f3e3d9eb8121c727e77e2
rajesh0411 <rajeshc0411@gmail.com>
2023-06-15 15:57:28 +0530

    Update XSS_level1.php

--- XSS/XSS_level1.php
+++ XSS/XSS_level1.php
@@ -15,6 +15,10 @@
    <p>Your name:<input type="text" name="username"></p>
    <input type="submit" name="submit" value="Submit">
 </form>
+   <form method="GET" action="" name="form">
+   <p>Your name:<input type="text" name="username"></p>
+   <input type="submit" name="submit" value="Submit">
+</form>	
 	</div>
 <?php
 if(isset($_GET["username"]))
